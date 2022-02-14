f101a0b92c5f849d2f74794d56aaf4c8404f097b
Victor Ro <vctr.ro@gmail.com>
2022-02-15 00:21:05 +0400

    Lab1.3 - Added solution Solved exercises 1, 2 and 3

---
+++ Lab1.3/INamed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1_3_LINQ
+{
+    public interface INamed
+    {
+        string Name { get; set; }
+    }
+
+    class Person : INamed
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
--- Lab1.3/Program.cs
+++ Lab1.3/Program.cs
@@ -1,12 +1,82 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Lab1_3_LINQ
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Person[] people = new[] {
+                new Person("Pit"),
+                new Person("Bob"),
+                new Person("Tyron"),
+                new Person("Mary"),
+                new Person("Victor"),
+                new Person("Igor"),
+                new Person("Max")
+            };
+
+            Console.WriteLine("\n----- Exercise 1 -----\n");
+            Console.WriteLine(GetAllWithoutFirst3(people, '-'));
+
+            Console.WriteLine("\n----- Exercise 2 -----\n");
+            var newPeople = GetAllWithNameLengthGreaterThenIndex(people);
+            newPeople.ForEach(np => Console.WriteLine(np.Name));
+
+            Console.WriteLine("\n----- Exercise 3 -----\n");
+            GroupWordsByLength("Это что же получается: ходишь, ходишь в школу, а потом бац - вторая смена");
+        }
+
+        public static string GetAllWithoutFirst3(IEnumerable<INamed> source, char delimeter)
+        {
+            return source
+                .Skip(3)
+                .Select(named => named.Name)
+                .Aggregate((x, y) => x + delimeter + y);
+        }
+
+        public static List<INamed> GetAllWithNameLengthGreaterThenIndex(IEnumerable<INamed> source)
+        {
+            return source
+                .Select((named, index) => named.Name.Length > index + 1 ? named : null)
+                .Where(n => n != null)
+                .ToList();
+        }
+
+        public static void GroupWordsByLength(string source)
+        {
+            var punctMarks = @" .,/'-""=+\><?:;!&)(*~`".ToCharArray();
+            var result = source
+                .Split(punctMarks)
+                .Where(s => s != "")
+                .Select(s => s.ToLower())
+                .OrderByDescending(s => s.Length)
+                .GroupBy(s => s.Length,
+                    (length, words) => new
+                    {
+                        Count = words.Count(),
+                        Length = length,
+                        Words = words
+                    })
+                .Select((group, index) => new
+                    {
+                        GroupId = index+1,
+                        group.Count,
+                        group.Length,
+                        group.Words
+                    })
+                .OrderByDescending(g => g.Count);
+
+            foreach (var group in result)
+            {
+                Console.Write("Группа {0}. Длина {1}. Количество {2}.\n", group.GroupId, group.Length, group.Count);
+                foreach (var item in group.Words)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
