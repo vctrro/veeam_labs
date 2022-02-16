03a6fecd71650cfb46592e3be106fc7f14281b2a
Victor Ro <vctr.ro@gmail.com>
2022-02-16 12:23:18 +0400

    Lab1.4 - Project created

---
+++ Lab1.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab1_4_String_Exception
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
