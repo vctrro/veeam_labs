607064d4d57d7a77ac34750df6aeb35a838d08d3
Victor Ro <vctr.ro@gmail.com>
2022-02-16 12:57:19 +0400

    Lab1.4 - Exercise 1 solved

--- Lab1.4/Program.cs
+++ Lab1.4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Lab1_4_String_Exception
 {
@@ -6,8 +7,35 @@ namespace Lab1_4_String_Exception
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n----- Exercise 1 -----\n");
+            string[] strings = { "Salı", "Вторник", "Mardi", "Τρίτη", "Martes", "יום שלישי", "วันอังคาร", "الثلاثاء", "日曜日" };
+
+            foreach (var str in strings)
+            {
+                var serializedStr = SerializeString(str);
+                foreach (var bt in serializedStr)
+                {
+                    Console.Write(bt + " ");
+                }
+                Console.WriteLine();
+
+                var deserializedStr = DeserializeString(serializedStr);
+                Console.WriteLine(deserializedStr);
+            }
+        }
+
+        public static byte[] SerializeString(string source)
+        {
+            return Encoding.Unicode.GetBytes(source);
+        }
+    
+        public static string DeserializeString(byte[] source)
+        {
+            var deserializedStr = Encoding.Unicode.GetChars(source);
+            StringBuilder result = new();
+            return result.Append(deserializedStr).ToString();
         }
     }
+
 }
 
