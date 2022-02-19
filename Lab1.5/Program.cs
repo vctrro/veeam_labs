eb5e8edb6e148299bfb39f9979a8e587b008ae93
Victor Ro <vctr.ro@gmail.com>
2022-02-19 20:34:06 +0400

    Lab1.5 - Project created

---
+++ Lab1.5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab1_5_IDisposable
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
